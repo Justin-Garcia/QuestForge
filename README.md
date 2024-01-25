9309fa713895a92156ffde37a4b3721b01d014a0
Justin Garcia <justingarcia1901@gmail.com>
2024-01-24 21:43:43 -0500

    groundwork

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # QuestForge
 Designing a web application for task management/note taking. Design inspo: Retro gaming; Idea inspo: messy life :)
+
+FUTURE DEVELOPMENT: Have the application be multi-layered, Focus on making a "shareable tasks" function where users can also access group tasks
---
+++ models/Tasks.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const { type } = require('os');
+
+
+
+Task.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'user',
+              key: 'id',
+            },
+    },
+  },
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+module.exports = User;

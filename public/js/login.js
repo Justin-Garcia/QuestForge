d6fc4b6f06d3fdbc274a90b7507e944e71645dee
Justin Garcia <justingarcia1901@gmail.com>
2024-02-05 02:50:39 -0500

    separated login/signup form

--- public/js/login.js
+++ public/js/login.js
@@ -12,7 +12,6 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      // If successful, redirect the browser to the profile page
       document.location.replace('/homepage');
     } else {
       alert(response.statusText);
@@ -36,7 +35,7 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/homepage');
     } else {
       alert(response.statusText);
     }
